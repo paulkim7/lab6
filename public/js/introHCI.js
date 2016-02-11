46e620afa2d12ffd6a75d2ad3d2b39d7454bbfef
Paul Kim <paulkimx94@gmail.com>
2016-02-11 04:02:39 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,22 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, addProject);
+
+	console.log("/project/" + idNumber);
 	console.log("User clicked on project " + idNumber);
+}
+
+function addProject(result) {
+	console.log(result);
+
+	var projectHTML = 
+	'<img src="' + result.image + '" class="detailsImage">' +
+    '<h2>' + result.title + '</h2>' +
+    '<p><small>' + result.date + '</small></p></a>' +
+    '<br><p>' + result.summary + '</p>';
+
+	$("#project" + result.id +  " .details").html(projectHTML);
 }
 
 /*
@@ -34,5 +49,18 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+	e.preventDefault();
+
+	$.get("/palette", changeColors);
+
 	console.log("User clicked on color button");
+}
+
+function changeColors(result) {
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
